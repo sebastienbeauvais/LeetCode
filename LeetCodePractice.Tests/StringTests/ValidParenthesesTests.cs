9b6db93668be23be3427e59ff29a2a08b8a707b0
Sebastien Beauvais <sbeauvais7@gmail.com>
2026-02-14 14:45:07 -0800

    Added unit tests for IsValid

---
+++ LeetCodePractice.Tests/StringTests/ValidParenthesesTests.cs
@@ -0,0 +1,41 @@
+using LeetCodePractice.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Company.TestProject1;
+
+[TestClass]
+public class ValidParenthesesTests
+{
+    public readonly StringQuestions stringQuestions = new StringQuestions();
+    [TestMethod]
+    public void ValidParentheses_ShouldReturnTrue_WhenParenthesesAreValid()
+    {
+        var parentheses = "()";
+        var result = stringQuestions.IsValid(parentheses);
+        Assert.IsTrue(result);
+    }
+    public void ValidParentheses_ShouldReturnTrue_WithMixedParentheses()
+    {
+        var parentheses = "()[]{}";
+        var result = stringQuestions.IsValid(parentheses);
+        Assert.IsTrue(result);
+    }
+    public void ValidParentheses_ShouldReturnFalse_WhenParenthesesAreNotValid()
+    {
+        var parentheses = "(]";
+        var result = stringQuestions.IsValid(parentheses);
+        Assert.IsFalse(result);
+    }
+    public void ValidParentheses_ShouldReturnTrue_WhenParenthesesAreNested()
+    {
+        var parentheses = "([])";
+        var result = stringQuestions.IsValid(parentheses);
+        Assert.IsTrue(result);
+    }
+    public void ValidParentheses_ShouldReturnFalse_WhenParenthesesAreNestedAndInvalid()
+    {
+        var parentheses = "([})";
+        var result = stringQuestions.IsValid(parentheses);
+        Assert.IsFalse(result);
+    }
+}
---
+++ LeetCodePractice/Problems/StringQuestions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LeetCodePractice.Problems
+{
+    public class StringQuestions
+    {
+        public bool IsValid(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
