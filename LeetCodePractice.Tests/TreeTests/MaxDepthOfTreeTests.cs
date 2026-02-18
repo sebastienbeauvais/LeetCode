510c51df3f1b08c07b8fd3be55c248d7b1b3306a
Sebastien Beauvais <Sbeauvais7@gmail.com>
2026-02-18 08:08:43 -0800

    Created problem but need to upgrade version of visual studio to run tests using .NET 10

---
+++ LeetCodePractice.Tests/TreeTests/MaxDepthOfTreeTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodePractice.Tests.TreeTests
+{
+    [TestClass]
+    public class MaxDepthOfTreeTests
+    {
+    }
+}
---
+++ LeetCodePractice/Problems/TreeQuestions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCodePractice.Classes;
+
+namespace LeetCodePractice.Problems
+{
+    public class TreeQuestions
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            /*
+             * Given the root of a binary tree, return its maximum depth.
+             * A binary tree's maximum depth is the number of nodes along the 
+             * longest path from the root node down to the farthest leaf node.
+             * 
+             * EX1: root = [3,9,20,null,null,15,7]
+             *       3
+             *      / \
+             *     9   20
+             *        /  \
+             *       15   7
+             * Output: 3 (3->20->7)
+             */
+        }
+    }
+}
