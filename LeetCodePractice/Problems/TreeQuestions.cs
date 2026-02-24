453a679b08fe3455e170ce31232788a2efbda600
Sebastien Beauvais <Sbeauvais7@gmail.com>
2026-02-24 08:50:53 -0800

    Added problem details to IsSameTree

--- LeetCodePractice/Problems/TreeQuestions.cs
+++ LeetCodePractice/Problems/TreeQuestions.cs
@@ -45,8 +45,14 @@ namespace LeetCodePractice.Problems
         public bool IsSameTree(TreeNode p, TreeNode q) 
         {
             /*
-             * 
-             * 
+             * Given the roots of two binary trees p and q, write a function to check if 
+             * they are the same or not.Two binary trees are considered the same if they 
+             * are structurally identical, and the nodes have the same value.
+             * EX1: p = [1,2,3], q = [1,2,3]
+             *       1          1
+             *      / \        / \
+             *     2   3      2   3
+             * Output: true
              */
 
             throw new NotImplementedException ();
